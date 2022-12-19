a010cd4c3e60c9f86860758922844c42b23d70ac
ArxFatal <thegremobgames@gmail.com>
2022-12-19 17:27:35 +0700

    Исправление запроса в файле changeUserProcess.php

--- supportfiles/changeUserProcess.php
+++ supportfiles/changeUserProcess.php
@@ -9,7 +9,7 @@
   $type = $_POST['type'];
   $id = $_POST['id'];
 
-  $q = "UPDATE `users` SET `login` = '$login', `password` = '$password', `email` = '$email', `type` = '$type' WHERE `users`.`id` = '$id'";
+  $q = "UPDATE `users` SET `login` = '$login', `password` = '$password', `email` = '$email', `type` = '$type', `name` = '$name' WHERE `users`.`id` = '$id'";
   $result = mysqli_query($connection, $q);
 
   header("Location:../../addUser.php");
